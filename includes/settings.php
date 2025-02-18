57032d160fc0a11040276064f266f081e3212f12
izmeera2000 <79130602+izmeera2000@users.noreply.github.com>
2025-02-18 17:09:27 +0800

    asdsa
    dasdas

--- includes/settings.php
+++ includes/settings.php
@@ -2,6 +2,6 @@
 
 
 $basePath = 'OMG'; // Subfolder path
-$basePath2 = '/OMG'; // Subfolder path
+$basePath2 = ''; // Subfolder path
 $rootPath = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/') . '/';
 date_default_timezone_set('Asia/Kuala_Lumpur');
